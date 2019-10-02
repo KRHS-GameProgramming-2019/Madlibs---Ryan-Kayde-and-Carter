6f11ca3c5df5c0258c04987a0c677ddc6dd70d78
Ryan St. Lawrence <ryan.stlawrence@kearsarge.org>
2019-10-02 11:32:40 -0400

    added

--- Story3.py
+++ Story3.py
@@ -4,20 +4,43 @@ def Story3(debug = False):
     if debug: print("Story1 Function")
     
     print("\n")
-    friendName1 = getWord("Enter a name: ", debug)
-    sport1 = getSport("Enter a sport: ", debug)
-    adjective1 = getAdjective("Enter an adjective: ", debug)
-    adjective2 = getAdjective("Enter another adjective: ", debug)
-    place1 = getPlace("Enter a place: ", debug)
-    place2 = getPlace("Enter another place: ", debug)
-    food1 = getFood("Enter a food: ", debug)
-    youtuber1 = getYou("Enter a youtuber: ", debug)
-    youtuber2 = getYou("Enter another youtuber: ", debug)
-    videoGame1 = getGame("Enter a videogame: ", debug)
     weekDay1 = getDay("Enter a weekday: ", debug)
+    marioCharacter1 = getcharacter("Enter a character: ", debug)
     
     
     
     
     out = "\n"
-    out += "Today was " + weekDay1
+    out += "Today was " + weekDay1 + ","
+    out += "\n"
+    out += "Mario kart day!"
+    out += "\n"
+    out += "my friends and I got our controllers and selected characters."
+    out += "Kaden wanted to play as " + marioCharacter1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return out
