781214a5d676aaad03096486304888b74ae1b574
DarkShock150 <carter.hasey@kearsarge.org>
2019-10-02 11:31:59 -0400

    added story

--- Story4.py
+++ Story4.py
@@ -1,1 +1,17 @@
 from Getword import *
+
+def Story2(debug = False):
+    if debug: print("Story1 Function")
+    
+    print("\n")
+    friendName1 = getWord("Enter a name: ", debug)
+    sport1 = getSport("Enter a sport: ", debug)
+    adjective1 = getAdjective("Enter an adjective: ", debug)
+    adjective2 = getAdjective("Enter another adjective: ", debug)
+    adjective3 = getAdjective("Enter another adjective: ", debug)
+    place1 = getPlace("Enter a place: ", debug)
+    place2 = getPlace("Enter another place: ", debug)
+    food1 = getFood("Enter a food: ", debug)
+    youtuber1 = getYou("Enter a youtuber: ", debug)
+    youtuber2 = getYou("Enter another youtuber: ", debug)
+    verb1 = getVerb("Enter a verb: ", debug)
