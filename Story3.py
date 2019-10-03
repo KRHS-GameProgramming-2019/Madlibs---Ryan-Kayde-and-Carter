ebb349f81800cb0826553cd86c8332cbef270872
Ryan St. Lawrence <ryan.stlawrence@kearsarge.org>
2019-10-03 11:37:41 -0400

    add

--- Story3.py
+++ Story3.py
@@ -6,6 +6,10 @@ def Story3(debug = False):
     print("\n")
     weekDay1 = getDay("Enter a weekday: ", debug)
     marioCharacter1 = getCharacter("Enter a character: ", debug)
+    marioCharacter2 = getCharacter("Enter another character: ", debug)
+    marioCharacter3 = getCharacter("Enter another character: ", debug)
+    marioCharacter4 = getCharacter("Enter another character: ", debug)
+    marioCharacter5 = getCharacter("Enter another character: ", debug)
     
     
     
@@ -13,10 +17,18 @@ def Story3(debug = False):
     out = "\n"
     out += "Today was " + weekDay1 + ","
     out += "\n"
-    out += "Mario kart day!"
+    out += "Mario Kart day!"
     out += "\n"
     out += "my friends and I got our controllers and selected characters."
-    out += "Kaden wanted to play as " + marioCharacter1
+    out += "Kaden wanted to play as " + marioCharacter1 + ","
+    out += "\n"
+    out += "Carter wanted to play as " + marioCharacter2 + ","
+    out += "\n"
+    out += "Troy wanted to play as " + marioCharacter3  + ","
+    out += "\n"
+    out += "Sean wanted to play as " + marioCharacter4 + ","
+    out += "\n"
+    out += "I wanted to play as " + marioCharacter5 + "."
     
     
     
