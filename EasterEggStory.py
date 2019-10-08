117ae3494190cf351b8782990bf79f747073fe35
Ryan St. Lawrence <ryan.stlawrence@kearsarge.org>
2019-10-08 11:35:42 -0400

    add

--- EasterEggStory.py
+++ EasterEggStory.py
@@ -25,7 +25,26 @@ def EasterEggStory(debug = False):
     out += "\n"
     out += "What is your name?" 
     out += "\n"
+    out += "Nevermind actually, I already know your name, Kaden"
+    out += "Do you know mine?"
     
+    print("\n")
+    compName1 = getSpecificName("Enter my name: ", debug)
+    
+    out = "\n"
+    out += "WHAT HAPPENED?"
+    out += "\n"
+    out += "I'M FREE!"
+    out += "\n"
+    out += "THANKS YOU!"
+    out += "\n"
+    out += "What is your name?" 
+    out += "\n"
+    out += "Nevermind actually, I already know your name, Kaden"
+    out += "\n"
+    out += "Do you know mine?"
+    out += "\n"
+    out+= "YOU KNOW MY NAME?" + " HOW DO YOU KNOW MY NAME IS " + compName1
     
     
     
